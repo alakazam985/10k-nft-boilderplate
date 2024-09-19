212aa24a1989d16c860ea982020858efdffb78d2
stean985 <146309116+stean985@users.noreply.github.com>
2024-09-19 12:34:07 +0100

    J'ai pu écrire le fichier de configuration de gardhat et lancer la compilation et la vérification du smart contract NFT

--- backend/ignition/modules/NFT.js
+++ backend/ignition/modules/NFT.js
@@ -4,7 +4,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("NFTModule", (m) => {
 
-  const nft = m.contract("CoreNFT", ["https://corenft.vercel.app/generated_metadata/"]);
+  const nft = m.contract("CoreNFT", ["https://github.com/alakazam985/nft-collection/tree/master/generated_metadata"]);
 
   return { nft };
 });
